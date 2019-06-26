de7cdf84c0445c46e9797e6c462859ffb90b1873
NaeemCho <naimul.chowdhury15@ncf.edu>
2019-06-26 04:31:17 -0400

    Initial Commit

---
+++ Ch1/Introductory_Code.py
@@ -0,0 +1,12 @@
+import nltk
+
+#Opens a client to allow downloading of several packages. This time only to download the books.
+nltk.download() 
+
+#gives the title of the first book, Moby Dick
+text1 
+
+#concordance gives every occurrence of a given word
+text1.concordance("monstrous")
+text2.concordance("affection")
+text6.concordance("silly")
