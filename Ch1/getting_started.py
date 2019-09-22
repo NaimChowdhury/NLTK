8e73d096b98a8707864572a1d78c07fe8305cc65
NaeemCho <naimul.chowdhury15@ncf.edu>
2019-09-21 20:25:59 -0400

    Added lines describing how to look up words in a text.

--- Ch1/getting_started.py
+++ Ch1/getting_started.py
@@ -1,4 +1,21 @@
 import nltk
 
 
+# from NLTK's book module, load all items
+from nltk.book import *
+
+# To find out about texts, enter the text names
+text1
+text2
+
+# concordance shows every occurence of a given word in a given text
+text1.concordance("monstrous")
+text2.concordance("affection")
+text3.concordance("lived")
+
+# 'text1 is moby dick'
+# 'text2 is Sense and Sensibility'
+# 'text3 is the Bible'
+# 'text4 is Inaugural Address Corpus'
+# 'text5 is NPS Chat Corpus, for unconvential words like im, ur, lol'
 
